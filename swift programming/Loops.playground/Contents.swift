781f09100c04b990a965900cf0f3b779ea1d8f40
Kalyan9493 <kalyankumarreddykavalakuntla@gmail.com>
2021-02-17 20:07:39 +0530

    added programs

--- swift programming/Loops.playground/Contents.swift
+++ swift programming/Loops.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
+// For in Loop
 
+for counter in 1...5{
+    print(counter)
+}
+
+
+let array = [10,20,30,40,50,60]
+
+for element in array{
+    print("\(element)")
+}
+
+
+// While Loop
+
+var i=0
+
+while i<array.count{
+    print("\(array.count)")
+    i+=1
+}
---
+++ swift programming/SwapNumbers.playground/Contents.swift
@@ -0,0 +1,11 @@
+var a = 10;
+var b = 20;
+
+print("Before Swapping : a =  \(a) , b = \(b)")
+
+a = a + b;
+b = a - b;
+a = a - b;
+
+
+print("After Swapping : a = \(a) , b =  \(b)")
